530fa490a0002508648c05762cd0aecf76b2f90f
Smythy <smythy.costa@hotmail.com>
2018-03-23 14:46:15 -0300

    m1 file2

--- M1/02-CRIAÇÃO DE USUÁRIOS E GRUPOS DEBIAN.py
+++ M1/02-CRIAÇÃO DE USUÁRIOS E GRUPOS DEBIAN.py
@@ -1,18 +1,15 @@
-LINUX DEBIAN
-
-	## O objetivo do grupo é da permissões a respectivos usuários.
-
-adduser paulo	   	=adiciona o usuário pedro
+## [grupo e permissões]
+adduser paulo	   	#adiciona o usuário pedro
 	
 	## Em seguida pedira senha
 	## confirmação de senha
 	## As outras informações podem ser deixadas em branco
-## Obs.: todos os usuários criados estão dentro da pasta /home.
+	## Obs.: todos os usuários criados estão dentro da pasta /home.
 
 
-addgroup nome_do_grupo            =adicionando grupo
-gpasswd -a paulo nome-do_grupo    =adicionando usuário ao grupo
-gpasswd -d paulo nome-do_grupo    =removendo usuário do grupo
+addgroup nome_do_grupo            #adicionando grupo
+gpasswd -a paulo nome-do_grupo    #adicionando usuário ao grupo
+gpasswd -d paulo nome-do_grupo    #removendo usuário do grupo
 
 
 cat /etc/group   		=verificando grupos
@@ -20,23 +17,23 @@ cat /etc/passwd			=visualiza os usuários do sistema
 cat /etc/shadow    		=visualizar senhas do usuários
 
 	
-	## criando usuários dentro de grupos
+## criando usuários dentro de grupos
 adduser - -ingroup grupo novo_usuario
 	
 
-	##  comando para que os usuários sejam criados com as pastas já gravadas.
+##  comando para que os usuários sejam criados com as pastas já gravadas.
 mkdir /etc/skel/{Downloads,Documentos,Musicas,Vídeos}
 
 
-	##  para criação de pastas em série ex:
+##  para criação de pastas em série ex:
 mkdir {pasta1,pasta2,pasta3,pasta4}
 
 
-	##  deletando usuarios.
+##  deletando usuarios.
 userdel -r  	=apaga tudo
 userdel     	=deleta o usuário mais suas pasta continuam 
 	
-	##  deletando grupos
+##  deletando grupos
 groupdel  =apagando grupos
 
 
